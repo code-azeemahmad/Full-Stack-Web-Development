3144768abaf787b6e747b8923093a1348b5716aa
code-azeemahmad <azeemahmadd9@gmail.com>
2025-07-26 10:16:50 +0500

    Console.log and some basics

--- Frontend/JavaScript/Introduction to JavaScript & Installing Node.js/script.js
+++ Frontend/JavaScript/Introduction to JavaScript & Installing Node.js/script.js
@@ -1,1 +1,17 @@
-alert("Hello World")
+alert("Hello World ")
+
+console.log("Code is Running")
+console.log("Code is looking like a wow")
+console.log("Code is Running swiftly")
+
+var a = prompt("Enter a number: ")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer?")
+
+if(isTrue) {
+    console.log("Computer is blasting!")
+}
+else{
+    console.log("Computer isn't blasting.")
+}
+
+console.log("Your number is " + a)
