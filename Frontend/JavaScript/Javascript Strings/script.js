ebf77aaa8a644328aa2fb8551d67e9cff46486c8
code-azeemahmad <azeemahmadd9@gmail.com>
2025-07-28 12:31:28 +0500

    Accessing Characters in a String

---
+++ Frontend/JavaScript/Javascript Strings/script.js
@@ -0,0 +1,10 @@
+console.log("I am learning Strings Tutorial");
+
+let a = "Azeem";
+
+console.log(a[0]);
+console.log(a[1]);      //"JavaScript is considered a forgiving or 'organic' language because 
+console.log(a[2]);      //it tries to avoid throwing errors whenever possible.
+console.log(a[3]);      //Instead, it often attempts to continue execution 
+console.log(a[4]);      //by automatically correcting or ignoring minor mistakes."
+console.log(a[5]);     
