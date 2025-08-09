8af32a176f60541451fe334ff9bc3005a4225494
code-azeemahmad <azeemahmadd9@gmail.com>
2025-08-09 18:01:36 +0500

    Promise.properties()

--- Frontend/JavaScript/JavaScript Callbacks & Promises/JavaScript Promise.js
+++ Frontend/JavaScript/JavaScript Callbacks & Promises/JavaScript Promise.js
@@ -17,6 +17,42 @@ prom1.then((a) => {
     console.log(err)
 })
 
+let prom2 = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("No random number was supportinng you! 2");
+  } else {
+    setTimeout(() => {
+      console.log("Yes! I am done 2");
+      resolve("Harry 2");
+    }, 3000);
+  }                           
+});
+
+// let p3 = Promise.all([prom1, prom2])
+// p3.then((a) => {
+//   console.log(a);
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// let p3 = Promise.allSettled([prom1, prom2])
+// p3.then((a) => {
+//   console.log(a);
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+let p3 = Promise.race([prom1, prom2])
+p3.then((a) => {
+  console.log(a);
+}).catch((err) => {
+    console.log(err)
+})
 
 /* Promise chaining is when you use multiple .then() calls one after another so the output of one promise is passed to the next.
-It’s like saying: "Do this, then that, then that…" in a clean sequence.` */
+It’s like saying: "Do this, then that, then that…" in a clean sequence. */
+
+// attaching multiple handlers
+
+// there is a big difference between resolve and settle
