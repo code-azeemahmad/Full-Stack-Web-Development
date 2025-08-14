78f87c8c65b627fc6543b94b11c0c6ee9a50e84f
code-azeemahmad <azeemahmadd9@gmail.com>
2025-08-14 09:54:38 +0500

    Explanation of factorial using Array Methods

--- Frontend/JavaScript/JavaScript Exercise 11 - Calculate the Factorial/Calculate Factorial using Arrays.js
+++ Frontend/JavaScript/JavaScript Exercise 11 - Calculate the Factorial/Calculate Factorial using Arrays.js
@@ -16,3 +16,11 @@ function facFor(number){
 }
 console.log(factorial(a))
 console.log(facFor(a))
+
+
+//____________Explanation__________________
+
+console.log(Array(7))                         // This creates an empty array with length 7:
+console.log(Array(7).keys())                  // .keys() returns an iterator of the array's indexes:
+// Array.from(Array(7).keys())                // Array.from() takes something that’s array-like or iterable (like .keys()) and turns it into a real array:
+console.log(Array.from(Array(7).keys()))
