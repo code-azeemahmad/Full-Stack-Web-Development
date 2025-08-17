deb9270c3a94d9169af0204bc0a3427da2a1c2d6
code-azeemahmad <azeemahmadd9@gmail.com>
2025-08-17 07:24:31 +0500

    HTTP Methods and Response status codes

---
+++ Frontend/JavaScript/HTTP Methods/script.js
@@ -0,0 +1,51 @@
+// Request and Response
+
+// HTTP Verbs --> Hyper Text Transfer Protocol
+// Response Status Code
+// HTTP response headers also contain details about the responses such content type, HTTP status code etc.
+
+/*
+
+--> GET
+        The GET method requests a representation of the specified resource. Requests using GET should only retrieve data and should not contain a request content.
+
+--> HEAD
+        The HEAD method asks for a response identical to a GET request, but without a response body.
+
+--> POST
+        The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.
+
+--> PUT
+        The PUT method replaces all current representations of the target resource with the request content.
+
+--> DELETE
+        The DELETE method deletes the specified resource.
+
+--> CONNECT
+        The CONNECT method establishes a tunnel to the server identified by the target resource.
+
+--> OPTIONS
+        The OPTIONS method describes the communication options for the target resource.
+
+--> TRACE
+        The TRACE method performs a message loop-back test along the path to the target resource.
+
+--> PATCH
+        The PATCH method applies partial modifications to a resource.
+
+*/
+
+// generally, fetch api are GET requests.
+
+/*
+
+HTTP response status codes
+    HTTP response status codes indicate whether a specific HTTP request has been successfully completed. Responses are grouped in five classes:
+
+    --> Informational responses (100 – 199)
+    --> Successful responses (200 – 299)
+    --> Redirection messages (300 – 399)
+    --> Client error responses (400 – 499)
+    --> Server error responses (500 – 599)
+
+*/
