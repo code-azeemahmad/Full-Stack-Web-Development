f437e9d685eabc259d03de119eca02828577033d
code-azeemahmad <azeemahmadd9@gmail.com>
2025-08-22 11:27:07 +0500

    Sending PUT Request

--- Backend/Express js/response-requests-and-routers-in-express/main.js
+++ Backend/Express js/response-requests-and-routers-in-express/main.js
@@ -5,16 +5,26 @@ const port = 3000;
 // Serve static files from "public" folder
 app.use(express.static("public"));
 
-// GET request
+// // GET request
 app.get("/", (req, res) => {
   console.log("Hey! It's a GET request");
-  res.send("Hello World!");
+  res.send("Hello World! GET");
 });
 
 // POST request
 app.post("/", (req, res) => {
   console.log("Hey! It's a POST request.");
-  res.send("Hello World!");
+  res.send("Hello World! POST");
 });
 
-// Start the ser
+// PUT request
+app.put("/", (req, res) => {
+  console.log("Hey! It's a PUT request.");
+  res.send("Hello World! PUT");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`🚀 Server running at http://127.0.0.1:${port}/`);
+});
+
