30695827867ad90b42b640e626630b3c9056eb4a
code-azeemahmad <azeemahmadd9@gmail.com>
2025-08-29 10:45:29 +0500

    Nav and Footer

---
+++ Backend/React/React-Components-Prop-JSX/first-App/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+      <div></div>
+    </>
+  )
+}
+
+export default App
+
+// F:/Sigma Web Development/Full-Stack-Web-Development/Backend/React/React-Components-Prop-JSX/first-App/
+// first-App is our current/ present working directory pwd
+
+// <div></div>, its not html, its jsx
+// wrap more than one div in a wrapper to return them
